a6fe9c9798908dab50b2c9519fcb456e62229581
u6870923 <Zheyuan.Zhang@anu.edu.au>
2019-09-24 22:09:28 +1000

    TileTest

---
+++ src/comp1110/ass2/TileTest.java
@@ -0,0 +1,21 @@
+package comp1110.ass2;
+
+import org.junit.jupiter.api.Test;
+
+public class TileTest {
+
+    @Test
+    public void testType(){
+
+    }
+
+    @Test
+    public void testOrientation(){
+
+    }
+
+    @Test
+    public void testPlacement2Location(){
+
+    }
+}
