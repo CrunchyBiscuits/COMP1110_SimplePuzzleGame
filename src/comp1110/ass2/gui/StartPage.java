8d3e74e3c764585289a4f8979d0d9d151ed29106
u6870923 <Zheyuan.Zhang@anu.edu.au>
2019-10-14 23:29:29 +1100

    update start page, update background music Zheyuan Zhang

--- src/comp1110/ass2/gui/StartPage.java
+++ src/comp1110/ass2/gui/StartPage.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 /**
  * author Zheyuan Zhang u6870923
@@ -14,20 +19,36 @@ public class StartPage extends Application {
     private static final int GAME_WIDTH = 933;
     private static final int GAME_HEIGHT = 700;
 
+    private static final String URI_BASE = "assets/";
+    private static final String IQFOCUS_URI = StartPage.class.getResource(URI_BASE + "iqfocus.png").toString();
+
     /* node groups */
     private final Group root = new Group();
+
     @Override
     public void start(Stage stage){
         stage.setTitle("FOCUSGAME - Fun with the Tiles");
-        Scene scene = new Scene(root,GAME_WIDTH, GAME_HEIGHT);
+        Scene scene = new Scene(root,GAME_WIDTH,GAME_HEIGHT);
 
         Button startBtn = new Button("New Game");
-        startBtn.setLayoutX(300);
-        startBtn.setLayoutY(300);
+        startBtn.setLayoutX(700);
+        startBtn.setLayoutY(50);
+
+        startBtn.setPrefSize(150,50);
+
         startBtn.setOnMousePressed(event -> {
-            new Board().start(stage);
+                new Board().start(stage);
         });
+
+        Button developerBtn = new Button("developers");
+        developerBtn.setLayoutX(700);
+        developerBtn.setLayoutY(120);
+
+        developerBtn.setPrefSize(150,50);
+
+        root.getChildren().add(new ImageView(IQFOCUS_URI));
         root.getChildren().add(startBtn);
+        root.getChildren().add(developerBtn);
         stage.setScene(scene);
         stage.show();
     }
